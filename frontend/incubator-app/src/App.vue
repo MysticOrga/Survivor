4707101c49ade1686fb9c9a801b6c1bb63eeb492
LoicR <loic.rabearivelo@epitech.eu>
2025-09-02 09:24:59 +0400

    feat(front): App route

---
+++ frontend/incubator-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to Incubator App</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/home/catalog">Catalog</router-link> |
+      <router-link to="/home/news">News</router-link> |
+      <router-link to="/home/events">Events</router-link>
+
+      | <router-link to="/admin/dashboard">Admin Dashboard</router-link>
+
+      | <router-link to="/startup/profile">Startup Profile</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
