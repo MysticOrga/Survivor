4eaba137004e80929c2a65a63509bce1c9287524
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2025-09-02 16:01:43 +0400

    [structure_news_pages]add: structure for the news feed pages + TODO: fix the page that doesn't display when we click on a new

--- frontend/incubator-app/src/views/home/NewsFeed.vue
+++ frontend/incubator-app/src/views/home/NewsFeed.vue
@@ -1,3 +1,30 @@
 <template>
-    <div class="">test</div>
+    <div class="feed">
+        <div
+        v-for="newsc in news"
+        :key="newsc.id"
+        >
+            <router-link :to="`/home/news/${newsc.id}`">
+                <div class="section">
+                    <h2>{{newsc.title}}</h2>
+                    <!-- <img></img> -->
+                    <p>{{newsc.hook}}</p>
+                    <p>{{newsc.text}}</p>
+                </div>
+            </router-link>
+        </div>
+    </div>
 </template>
+
+<script>
+import news from '@/example/news.json'
+
+export default {
+    name: 'News',
+    data() {
+        return {
+            news
+        }
+    }
+}
+</script>
---
+++ frontend/incubator-app/src/views/home/NewsPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <section v-if="newsc">
+        <div class="new">
+            <h2>{{newsc.title}}</h2>
+            <!-- <img></img> -->
+                    <p>{{newsc.hook}}</p>
+                    <p>{{newsc.text}}</p>
+            </div>
+        </section>
+    </section>
+    <section v-else>
+        <p>Pas de news</p>
+    </section>
+</template>
+
+<script>
+import news from '@/example/news.json'
+
+export default {
+  name: 'News',
+  data() {
+    return {
+      newsc: null
+    }
+  },
+  created() {
+    const id = Number(this.$route.params.id)
+    this.newsc = news.find(s => s.id === id)
+  }
+}
+</script>
