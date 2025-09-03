5890a57f624c1d2fc9c5ae7a62785e3481063ef6
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-03 22:31:21 +0400

    feat(Service): create a mongodb service

--- Src/Models/StartupModels.js
+++ Src/Models/StartupModels.js
@@ -1,41 +1,21 @@
 const { ObjectId } = require("mongodb");
-const client = require("../Config/db");
+const MongoAPI = require("../Services/MongoService")
 
 async function insertStartup(data) {
-    try {
-        const db = client.db("ClientDB");
-        const col = db.collection("startup");
-
-        if (Array.isArray(data)) {
-            return await col.insertMany(data);
-        } else {
-            return await col.insertOne(data);
-        }
-    } finally {
-        console.log("fini")
-    }
+    return await MongoAPI.createDocument("startup", data);
 }
 
 async function getStartup(id)
 {
-    try {
-        const db = client.db("ClientDB");
-        const col = db.collection("startup");
+    const filter = {
+        _id: new ObjectId(id)
+    };
 
-        return await col.find({ _id : new ObjectId(id)}).toArray();
-    } finally {
-        console.log("fini")
-    }
+    return await MongoAPI.readDocuments("startup", filter);
 }
 
 async function getAllStartups() {
-    try {
-        const db = client.db("ClientDB");
-        const col = db.collection("startup");
-        return await col.find({}).toArray();
-    } finally {
-        console.log("fini")
-    }
+    return await MongoAPI.readDocuments("startup", {});
 }
 
 module.exports = { insertStartup, getAllStartups, getStartup };
