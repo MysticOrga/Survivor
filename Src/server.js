384b58ace6484a3b9c9dcd7a62c2628fb51f343f
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-03 09:35:40 +0400

    feat(route): route to get all startup

--- Src/server.js
+++ Src/server.js
@@ -5,13 +5,14 @@ const app = express();
 const router = express.Router();
 const { startupRoute } = require("./Routes/StartupRoute");
 const { getAllStartups } = require("./Services/startupServices");
-router.get("/test", getAllStartups);
+
+router.get("/startup", async (req, res) => {
+    const startup = await getAllStartups();
+    console.log(startup);
+    res.send(startup)
+});
 
 app.use("/", router);
-
-app.get("/hello", (req, res) => {
-    res.send("hello boyao");
-})
 
 app.listen(PORT, () => {
     console.log(`listen on port ${PORT}`);
