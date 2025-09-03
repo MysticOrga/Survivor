94ec61ee6c5a20fe8a26de2279deac7ab6c46cc9
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2025-09-03 16:14:43 +0400

    [startup-profile]add: structure for the startup profile's page + TODO: CSS and possibility to modificate the information

--- frontend/incubator-app/src/views/startup/StartupProfile.vue
+++ frontend/incubator-app/src/views/startup/StartupProfile.vue
@@ -1,3 +1,31 @@
 <template>
-    <div class="">test</div>
+    <div class="profile">
+        <div v-for="info in profile">
+            <!-- <img></img> -->
+            <h2>{{ info.name }}</h2>
+            <p><strong>Fondateur(s):</strong> {{ info.founders }}</p>
+            <p><strong>Crée en/le:</strong> {{ info.created }}</p>
+            <p><strong>Localisation:</strong> {{ info.located }}</p>
+            <p><strong>Téléphone :</strong> {{ info.phone }}</p>
+            <p></p>
+            <p><strong>Capital:</strong> {{ info.capital }}</p>
+            <p><strong>Secteur:</strong> {{ info.sector }}</p>
+            <p><strong>SIRET:</strong> {{ info.SIRET }}</p>
+            <p><strong>Effectif(s):</strong> {{ info.effectif }}</p>
+            <p><strong>Vos projets:</strong> {{ info.project }}</p>
+        </div>
+    </div>
 </template>
+
+<script>
+import profile from '@/example/profile.json'
+
+export default {
+    name: "StartupProfile",
+    data() {
+        return {
+            profile
+        }
+    }
+}
+</script>
