696d4117647624bc46e4fdc39b05dd760c2632d5
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-03 09:11:10 +0400

    feat(route): add little express app

--- Src/Config/env.js
+++ Src/Config/env.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 module.exports = {
     PORT: process.env.PORT,
-    MONGO_URL: process.env.MONGO_URL
+    MONGO_URL: process.env.MONGO_URL,
+    CLIENT_API_KEY: process.env.CLIENT_API_KEY
 };
--- Src/server.js
+++ Src/server.js
@@ -1,22 +1,11 @@
-const client = require("./Config/db");
-const DBClient = require("./Config/db");
+const { PORT } = require("./Config/env");
+const MongoCli = require("./Config/db")
+const express = require("express");
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        const db = client.db("Survivor");
-        const obj = { test: "toto" };
-        await db.collection("user").insertOne(obj, function(err, db) {
-            if (err)
-                console.log("err");
-            else
-                console.log("good");
-        })
-        console.log("Succesful Connection");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
+app.get("/hello", (req, res) => {
+    res.send("hello boyao");
+})
+
+app.listen(PORT, () => {
+    console.log(`listen on port ${PORT}`);
+})
