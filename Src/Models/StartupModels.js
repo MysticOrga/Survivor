42297c64a1746d4b9feb02d6a204e3ccb58c200c
Nicolas SAMY <nicolas.samy@epitech.eu>
2025-09-04 09:27:36 +0400

    feat: adding views counter for startups

--- Src/Models/StartupModels.js
+++ Src/Models/StartupModels.js
@@ -7,26 +7,28 @@ async function insertStartup(data) {
         const col = db.collection("startup");
 
         if (Array.isArray(data)) {
+            data = data.map(d => ({ ...d, views: 0 }));
             return await col.insertMany(data);
         } else {
-            return await col.insertOne(data);
+            // return await col.insertOne(data);
+            return await col.insertOne({ ...data, views: 0});
         }
     } finally {
-        console.log("fini")
+        console.log("fini1")
     }
 }
 
-async function getStartup(id)
-{
-    try {
-        const db = client.db("ClientDB");
-        const col = db.collection("startup");
+// async function getStartup(id)
+// {
+//     try {
+//         const db = client.db("ClientDB");
+//         const col = db.collection("startup");
 
-        return await col.find({ _id : new ObjectId(id)}).toArray();
-    } finally {
-        console.log("fini")
-    }
-}
+//         return await col.find({ _id : new ObjectId(id)}).toArray();
+//     } finally {
+//         console.log("fini2")
+//     }
+// }
 
 async function getAllStartups() {
     try {
@@ -34,7 +36,22 @@ async function getAllStartups() {
         const col = db.collection("startup");
         return await col.find({}).toArray();
     } finally {
-        console.log("fini")
+        console.log("fini3")
+    }
+}
+
+async function getStartup(id) {
+    try {
+        const db = client.db("ClientDB");
+        const col = db.collection("startup");
+
+        return await col.findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            { $inc: { views: 1 } },
+            { returnDocument: "after" }
+        );
+    } finally {
+        console.log("fini4");
     }
 }
 
