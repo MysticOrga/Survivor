d0517daa6b5d0d58ae443ff364e0ece8aab3b930
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-04 11:38:07 +0400

    feat(API): add login endpoint API

--- Src/Config/env.js
+++ Src/Config/env.js
@@ -4,5 +4,8 @@ module.exports = {
     PORT: process.env.PORT,
     MONGO_URL: process.env.MONGO_URL,
     CLIENT_API_KEY: process.env.CLIENT_API_KEY,
-    DB_NAME: process.env.DB_NAME
+    DB_NAME: process.env.DB_NAME,
+    PASSWORD: process.env.PASSWORD,
+    SALTROUND: parseInt(process.env.SALTROUND, 10),
+    SECRET: process.env.SECRET
 };
--- Src/server.js
+++ Src/server.js
@@ -6,11 +6,14 @@ const app = express();
 const StartupRoute = require("./Controllers/StartupController");
 const NewsRoute = require("./Controllers/NewsController");
 const EventRoute = require("./Controllers/EventsController");
+const UserRoute = require("./Controllers/UserController");
 
+app.use(express.json());
 app.use(cors());
 app.use("/startups", StartupRoute);
 app.use("/news", NewsRoute);
 app.use("/events", EventRoute);
+app.use("/users", UserRoute);
 
 
 try {
