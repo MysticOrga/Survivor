eec4a37631e9447b6e991292841af3cfe1583b25
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2025-09-04 13:38:26 +0400

    [main-backend]fix: nme of db collection from new to event

--- Src/Models/EventsModels.js
+++ Src/Models/EventsModels.js
@@ -9,9 +9,9 @@ async function getAllEvent(data) {
 async function getEventId(id) {
     try {
         const db = client.db("ClientDB");
-        const col = db.collection("new");
+        const col = db.collection("event");
         // return await col.findOne( { _id, const name = new type(arguments);})
-        return await col.findOne( {_id: new ObjectId(id)} );
+        return await col.find( {_id: new ObjectId(id)} ).toArray();
     } finally {
         console.log("fini");
     }
