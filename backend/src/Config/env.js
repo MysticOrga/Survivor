807e71bc998bc29ed031aeeafb5a7bb32a1bef7c
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-05 11:09:02 +0400

    rebase structure folder

---
+++ backend/src/Config/env.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    MONGO_URL: process.env.MONGO_URL,
+    CLIENT_API_KEY: process.env.CLIENT_API_KEY,
+    DB_NAME: process.env.DB_NAME,
+    PASSWORD: process.env.PASSWORD,
+    SALTROUND: parseInt(process.env.SALTROUND, 10),
+    SECRET: process.env.SECRET
+};
