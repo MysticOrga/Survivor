4f47b8d35675b615d6a58b731979c9f6f716fe10
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-05 15:43:57 +0400

    fix(frontend): test startups route

--- frontend/src/views/home/ProjectCatalog.vue
+++ frontend/src/views/home/ProjectCatalog.vue
@@ -28,7 +28,7 @@ export default {
   },
   async created() {
     try {
-      const response = await axios.get('http://localhost:5001/startups');
+      const response = await axios.get('jeb-inucbator-api.vercel.app/startups');
       this.startups = response.data;
       this.originalStartups = [...response.data];
       console.log('Startups loaded:', this.startups);
