b8dbb6c461825dc00ba60e00d5ac66c79c25b987
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-07 17:04:41 +0400

    feat(DevAPI): read endpoints

--- backend/src/Services/DevService.js
+++ backend/src/Services/DevService.js
@@ -5,6 +5,7 @@ const express = require("express");
 const router = express.Router();
 const keyType = "dev";
 const User = require("../Models/UserModels");
+const { ObjectId } = require("mongodb");
 
 router.post("/user", authAPI(keyType), (req, res) => {
     const user = req.body;
@@ -26,3 +27,53 @@ router.post("/new", authAPI(keyType), (req, res) => {
 router.post("/startup", authAPI(keyType), (req, res) => {
     MongoAPI.createDocument("startup", req.body);
 })
+
+router.get("/users", async (req, res) => {
+    const data = await MongoAPI.readDocuments("user", {});
+    res.json(data);
+})
+
+router.get("/startups", async (req, res) => {
+    const data = await MongoAPI.readDocuments("startup", {});
+    res.json(data);
+})
+
+router.get("/news", async (req, res) => {
+    const data = await MongoAPI.readDocuments("new", {});
+    res.json(data);
+})
+
+router.get("/events", async (req, res) => {
+    const data = await MongoAPI.readDocuments("event", {});
+    res.json(data);
+})
+
+router.get("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id)};
+    const data = await MongoAPI.readDocuments("user", filter);
+    res.json(data);
+})
+
+router.get("/startups/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id)};
+    const data = await MongoAPI.readDocuments("startup", filter);
+    res.json(data);
+})
+
+router.get("/news/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id)};
+    const data = await MongoAPI.readDocuments("new", filter);
+    res.json(data);
+})
+
+router.get("/events/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id)};
+    const data = await MongoAPI.readDocuments("event", filter);
+    res.json(data);
+})
+
+module.exports = router;
