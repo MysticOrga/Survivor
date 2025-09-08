492723d5b129161fe0e17c50a712fb90ac3f442e
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-09-08 22:53:04 +0400

    feat(project): add crud for project

--- backend/src/Models/ProjectModel.js
+++ backend/src/Models/ProjectModel.js
@@ -1,6 +1,10 @@
 const { ObjectId } = require("mongodb");
 const MongoAPI = require("../Services/MongoService");
 
+exports.createProject = async (data) => {
+    return MongoAPI.createDocument("project", data);
+}
+
 exports.getProjects = async () => {
     return MongoAPI.readDocuments('project', {});
 }
@@ -8,4 +12,12 @@ exports.getProjects = async () => {
 exports.getProject = async (id) => {
     const filter = { _id: new ObjectId(id) };
     return MongoAPI.readDocuments('project', filter);
+}
+
+exports.updateProject = async (filter, data) => {
+    return MongoAPI.updateDocument("project", filter, data);
+}
+
+exports.deleteProject = async (filter) => {
+    return MongoAPI.deleteDocuments("project", filter);
 }
