0b921328668073a2f0b58db7a485b50c7f25a692
Nicolas SAMY <nicolas.samy@epitech.eu>
2025-09-09 09:33:18 +0400

    feat: simple filter for startup

--- backend/src/Controllers/StartupController.js
+++ backend/src/Controllers/StartupController.js
@@ -7,6 +7,26 @@ router.get("/", async (req, res) => {
     res.json(data);
 })
 
+router.get("/filter", async (req, res) => {
+    try {
+        const filters = {};
+
+        if (req.query.sector)
+            filters.sector = req.query.sector;
+        if (req.query.country)
+            filters.country = req.query.country;
+        if (req.query.stage)
+            filters.stage = req.query.stage;
+        const data = await Startup.getStartupByFilter(filters);
+        if (data.length > 0)
+            res.json(data);
+        else
+            res.status(404).send("No startups found for given filters");
+    } catch (err) {
+        res.status(500).send("Server error: " + err.message);
+    }
+});
+
 router.get("/:id", async (req, res) => {
     const data = await Startup.getStartup(req.params.id);
 
