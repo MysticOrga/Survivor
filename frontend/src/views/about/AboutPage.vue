0ac74d1f7fb9d4b2521069592e028f69397800d6
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2025-09-09 15:02:59 +0400

    [37-feat-about-page]fix: structure and css

--- frontend/src/views/about/AboutPage.vue
+++ frontend/src/views/about/AboutPage.vue
@@ -2,10 +2,12 @@
   <div class="about-page">
     <h2>Mission & Vision</h2>
     <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, harum? Ad
-      consequuntur delectus qui inventore eius! Laudantium delectus quia
-      dignissimos. Eum beatae sunt voluptatum eaque ducimus illum aperiam eius
-      nam.
+      Our mission is to empower startups by connecting them with the right
+      investors, mentors, and ressources. We streve to create an ecosytem where
+      innovative ideas can grow ith impactful businesses. Our vision is to
+      become the leading hub where entrepreuneurs and investors build the future
+      together, fostering, innovation, driving sustainable growth, and shaping
+      tomorrow's economy.
     </p>
     <h2>Team</h2>
     <p>
@@ -21,15 +23,19 @@
       dignissimos. Eum beatae sunt voluptatum eaque ducimus illum aperiam eius
       nam.
     </p>
-    <div class="contact-section">
-      <h2>Contact</h2>
-      <div class="contact-contact">
-        <h3>Contact</h3>
-        <p>0692 xx xx xx</p>
-        <p>184, JEB Incubator Street, 40404 JEB city</p>
-        <p>jeb.incubator@example.com</p>
+    <h2>Contact</h2>
+    <div class="contact">
+      <div
+        class="contact-section"
+        v-for="contact in contacts"
+        :key="contact.id"
+      >
+        <img class="contact-img" src="@/assets/pictures/contact.png"/>
+        <h3>{{ contact.name }}</h3>
+        <p>{{ contact.job }}</p>
+        <p>{{ contact.contact }}</p>
       </div>
-      <div class="contact-nav">
+      <!-- <div class="contact-nav">
         <h3>Navigation</h3>
         <router-link :to="`/`">
           <p>Home</p>
@@ -43,14 +49,21 @@
         <router-link :to="`/home/events`">
           <p>Events</p>
         </router-link>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
+import contacts from "@/example/contact.json";
+
 export default {
   name: "AboutPage",
+  data() {
+    return {
+      contacts,
+    };
+  },
 };
 </script>
 
@@ -64,7 +77,25 @@ export default {
   text-align: center;
 }
 
-.about-page .contact-section .contact-nav {
-    text-align: left;
+.contact {
+  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 1.5rem;
+  padding: 20px;
+}
+
+.contact-section{
+  display: grid;
+  box-shadow: 0 6px 14px rgba(0, 0, 0, 0.08);
+}
+
+.contact-section h3 {
+  text-align: center;
+}
+
+.contact-img {
+  height: 60px;
+  width: auto;
 }
 </style>
