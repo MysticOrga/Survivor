74cac691621693aed960802a2f43258be451461d
Nicolas SAMY <nicolas.samy@epitech.eu>
2025-09-09 16:33:53 +0400

    feat: filters for projects

--- backend/src/Controllers/ProjectController.js
+++ backend/src/Controllers/ProjectController.js
@@ -6,6 +6,28 @@ const auth = require("../Middlewares/auth");
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../Config/env");
 
+router.get("/filter", async (req, res) => {
+    try {
+        const filters = {};
+        for (const [key, value] of Object.entries(req.query)) {
+            if (key === "country") {
+                filters["address"] = { $regex: value + "$", $options: "i" };
+            } else if (key === "sector" || key === "maturity") {
+                filters[key] = { $regex: "^" + value + "$", $options: "i" };
+            } else {
+                filters[key] = value;
+            }
+        }
+        const data = await Startup.getStartupByFilter(filters);
+        if (data.length > 0)
+            res.json(data);
+        else
+            res.status(404).send("No startups found for this filters");
+    } catch (err) {
+        res.status(500).send("Server error: " + err.message);
+    }
+});
+
 router.get("/", async (req, res) => {
     const data = await Project.getProjects();
     res.json(data);
@@ -48,5 +70,8 @@ router.delete("/:id", auth("investor"), async (req, res) => {
     Project.deleteProject(filter);
     res.json({ msg: "project succesfully deleted"});
 })
+
+
+
 
 module.exports = router;
