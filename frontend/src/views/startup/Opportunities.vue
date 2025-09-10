7c55ba6defd5fc9ad13864c0d893a3bf2b83d191
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2025-09-10 15:17:41 +0400

    [opportunities-tracking-page]add: just title

--- frontend/src/views/startup/Opportunities.vue
+++ frontend/src/views/startup/Opportunities.vue
@@ -1,8 +1,27 @@
 <template>
     <nav>
-        <router-link to="/startup/profile">Profile</router-link>
-        <router-link to="/startup/messaging">Chat</router-link>
+        <router-link to="/startup/profile">Profile</router-link> |
+        <router-link to="/startup/messaging">Chat</router-link> |
         <router-link to="/startup/opportunities">Opportunities</router-link>
     </nav>
-    <div class="">Opportunities</div>
+    <div class="opportunities-css">
+        <h1>Calls for project</h1>
+        <h1>Funding opportunities</h1>
+        <h1>Investors matches</h1>
+    </div>
 </template>
+
+<script>
+
+export default {
+    name: "Opportunities",
+}
+
+</script>
+
+<style>
+
+.opportunities-css h1{
+    color: #CB90F1;
+}
+</style>
